c77eb02860b64cbd9e1fc2879268a022c9028336
Jon Croce <crocedev@gmail.com>
2023-01-31 00:21:01 -0500

    add seed data for prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,107 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+
+	// Create some Cats
+	const muffinCat = await prisma.cat.create({
+		data: { name: 'Muffin' }
+	});
+
+	const cupcakeCat = await prisma.cat.create({
+		data: { name: 'Cupcake' }
+	});
+
+	// Create some Caretakers
+	const dianeCaretaker = await prisma.caretaker.create({
+		data: { name: 'Diane' }
+	});
+
+	const busterCaretaker = await prisma.caretaker.create({
+		data: { name: 'Buster' }
+	});
+
+	// Create some FoodBrands
+	const friskiesFoodBrand = await prisma.foodBrand.create({
+		data: { name: 'Friskies' }
+	});
+
+	const craveFoodBrand = await prisma.foodBrand.create({
+		data: { name: 'Crave' }
+	});
+
+	// Create some FoodUnits
+	const canFoodUnit = await prisma.foodUnit.create({
+		data: {
+			name: 'Can',
+			minFractionalAmount: 0.5
+		}
+	});
+
+	const cupFoodUnit = await prisma.foodUnit.create({
+		data: {
+			name: 'Cup',
+			minFractionalAmount: 0.25
+		}
+	});
+
+	// Create some Foods
+	const friskiesChickenShreds = await prisma.food.create({
+		data: {
+			name: "Friskies Chicken Shreds",
+			brand: {
+				connect: friskiesFoodBrand
+			},
+			unit: {
+				connect: canFoodUnit
+			},
+			wetOrDry: 'Wet',
+			form: 'Shreds',
+			flavor: 'Chicken',
+			unitsInStock: 72
+		}
+	});
+
+	const friskiesWhitefishShreds = await prisma.food.create({
+		data: {
+			name: "Friskies Whitefish Shreds",
+			brand: {
+				connect: friskiesFoodBrand
+			},
+			unit: {
+				connect: canFoodUnit
+			},
+			wetOrDry: 'Wet',
+			form: 'Shreds',
+			flavor: 'Whitefish',
+			unitsInStock: 28
+		}
+	});
+
+	const craveSalmonKibble = await prisma.food.create({
+		data: {
+			name: "Crave Salmon Kibble",
+			brand: {
+				connect: craveFoodBrand
+			},
+			unit: {
+				connect: cupFoodUnit
+			},
+			wetOrDry: 'Dry',
+			form: 'Kibble',
+			flavor: 'Salmon',
+			unitsInStock: 51.75
+		}
+	});
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e: unknown) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
