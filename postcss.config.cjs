83a262bb3d612357730e606c80b66471f42b7d64
Jon Croce <crocedev@gmail.com>
2023-02-01 07:46:17 -0500

    add postcss-nested library

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,12 +1,11 @@
+const postcssNested = require('postcss-nested');
 const postcssPresetEnv = require('postcss-preset-env');
 
 const config = {
 	plugins: [
+		postcssNested(),
 		postcssPresetEnv({
 			stage: 2,
-			features: {
-				'nesting-rules': true
-			}
 		})
 	]
 };
