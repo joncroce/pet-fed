7ee70d26470ddec27a266e9872a9e55a06659cd1
Jon Croce <crocedev@gmail.com>
2023-02-02 09:30:25 -0500

    add initial dashboard view and subroutes

---
+++ src/routes/(protected)/dashboard/+layout.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import LogoSVG from '$lib/components/LogoSVG.svelte';
+
+	$: breadcrumbs = $page.url.pathname.split('/').slice(1);
+</script>
+
+<svelte:head>
+	<title>Pet Fed — Dashboard</title>
+</svelte:head>
+
+<header>
+	<div class="content-wrapper">
+		<div class="logo">
+			<a href="/"><LogoSVG /></a>
+		</div>
+		<div class="user-info">
+			{#if $page.data.user}
+				<span>Logged in as <strong>{$page.data.user.name}</strong></span>
+				<form action="/logout" method="POST">
+					<button class="button" type="submit">Logout</button>
+				</form>
+			{/if}
+		</div>
+	</div>
+</header>
+
+<nav>
+	<ol class="breadcrumbs">
+		{#each breadcrumbs as crumb, i}
+			<li>
+				<a href={`/${breadcrumbs.slice(0, i + 1).join('/')}`}
+					>{`${crumb[0].toUpperCase()}${crumb.slice(1)}`}</a
+				>
+			</li>
+		{/each}
+	</ol>
+</nav>
+<slot />
+
+<style lang="postcss">
+	header {
+		padding-block: 0.5rem;
+		padding-inline: 1rem;
+
+		.content-wrapper {
+			margin: 0 auto;
+			max-width: var(--width-content-max);
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+
+			.logo {
+				display: flex;
+				padding-inline: 1rem;
+				* {
+					fill: var(--color-tan-light);
+				}
+			}
+
+			.user-info {
+				font-family: sans-serif;
+				display: flex;
+				justify-content: flex-end;
+				align-items: flex-end;
+				gap: 0.5rem;
+
+				span {
+					font-size: 1.4rem;
+					color: var(--color-tan-light);
+				}
+
+				button {
+					font-size: 1.3rem;
+				}
+			}
+		}
+	}
+
+	nav {
+		border-bottom: 8px solid var(--color-tan);
+		background: var(--color-tan);
+
+		ol.breadcrumbs {
+			max-width: var(--width-content-max);
+			display: flex;
+			justify-content: flex-start;
+			align-items: center;
+			margin: 0 auto;
+			padding-inline: 0.5rem;
+			gap: 0.25rem;
+			list-style: none;
+			font-family: 'Gantari', sans-serif;
+			line-height: 1;
+			font-weight: 600;
+
+			li:not(:first-of-type):before {
+				padding-inline: 0.5em;
+				content: '>';
+			}
+
+			li:last-of-type {
+				font-weight: 800;
+			}
+		}
+	}
+</style>
--- src/routes/(protected)/dashboard/+page.svelte
+++ src/routes/(protected)/dashboard/+page.svelte
@@ -1,18 +1,99 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-
-	$: ({ pets } = data);
+	import Icon from '@iconify/svelte';
 </script>
 
-<h1>Dashboard</h1>
+<main>
+	<nav>
+		<ul>
+			<li>
+				<a href="/dashboard/pets"
+					><div class="header">Pets</div>
+					<div class="icon"><Icon icon="teenyicons:paws-solid" /></div></a
+				>
+			</li>
+			<li>
+				<a href="/dashboard/foods"
+					><div class="header">Foods</div>
+					<div class="icon"><Icon icon="game-icons:opened-food-can" /></div></a
+				>
+			</li>
+			<li>
+				<a href="/dashboard/analytics"
+					><div class="header">Analytics</div>
+					<div class="icon">
+						<Icon icon="teenyicons:area-chart-alt-solid" />
+					</div></a
+				>
+			</li>
+			<li>
+				<a href="/dashboard/households"
+					><div class="header">Households</div>
+					<div class="icon">
+						<Icon icon="teenyicons:house-solid" />
+					</div></a
+				>
+			</li>
+		</ul>
+	</nav>
+</main>
 
-{#if $page.data.user}
-	<p>Welcome {$page.data.user.name}!</p>
-{/if}
+<style lang="postcss">
+	main {
+		margin: 0 auto;
+		display: flex;
 
-{#if pets}
-	There are pets! {JSON.stringify(pets, undefined, 2)}
-{/if}
+		nav {
+			padding-block: 2rem;
+
+			ul {
+				list-style: none;
+				display: grid;
+				grid-template-columns: repeat(2, max(25vw, 400px));
+				gap: 1rem;
+
+				li {
+					width: 100%;
+					aspect-ratio: 16 / 9;
+					border-style: solid;
+					border-color: var(--color-chocolate-dark);
+					border-width: 3px 4px 4px 3px;
+					border-radius: 8px;
+					background-color: var(--color-gold);
+					box-shadow: 1.9px 1.4px 3.4px rgba(0, 0, 0, 0.034), 4.7px 3.3px 8.2px rgba(0, 0, 0, 0.048),
+						8.8px 6.3px 15.4px rgba(0, 0, 0, 0.06), 15.6px 11.2px 27.5px rgba(0, 0, 0, 0.072),
+						29.2px 20.9px 51.4px rgba(0, 0, 0, 0.086);
+
+					a {
+						display: flex;
+						flex-direction: column;
+						align-items: stretch;
+						text-decoration: none;
+						height: 100%;
+
+						.header {
+							display: flex;
+							justify-content: center;
+							padding-block: 0.25em;
+							padding-inline: 0.5em;
+							border-bottom: 2px solid var(--color-chocolate-dark);
+							font-size: 2rem;
+							font-family: 'Trochhi', serif;
+							font-weight: 600;
+							line-height: 1;
+							color: black;
+						}
+
+						.icon {
+							display: flex;
+							justify-content: center;
+							align-items: center;
+							flex: 1;
+							font-size: 7rem;
+							color: var(--color-mauve);
+						}
+					}
+				}
+			}
+		}
+	}
+</style>
