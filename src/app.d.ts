4dffbad856149014a29dbad4e3750405b0f28241
Jon Croce <crocedev@gmail.com>
2023-02-03 08:29:40 -0500

    add personId to locals object

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,6 +6,7 @@ declare global {
 		interface Locals {
 			user: {
 				name: string;
+				personId: string;
 			} | null;
 		}
 		// interface PageData {}
--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -9,12 +9,13 @@ export const authenticateUser = async (event: RequestEvent) => {
 	if (typeof authToken === 'string' && authToken.length) {
 		const user = await db.user.findUnique({
 			where: { authToken },
-			select: { username: true }
+			select: { username: true, person: true }
 		});
 
-		if (user) {
+		if (user && user.person) {
 			return {
-				name: user.username
+				name: user.username,
+				personId: user.person.id
 			};
 		}
 	}
