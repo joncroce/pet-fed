244bc116cfe521c4120714ae192651469a9f7f82
Jon Croce <crocedev@gmail.com>
2023-02-03 00:27:47 -0500

    update prisma schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,58 +3,10 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-	// Create a Species
-	const cat = await prisma.species.create({
-		data: {
-			name: 'Cat'
-		}
-	});
-
-	// Create some Pets
-	await prisma.pet.create({
-		data: {
-			name: 'Muffin',
-			species: { connect: { id: cat.id } }
-		}
-	});
-
-	await prisma.pet.create({
-		data: {
-			name: 'Cupcake',
-			species: { connect: { id: cat.id } }
-		}
-	});
-
-	// Create some Foods
-	await prisma.food.create({
-		data: {
-			name: 'Friskies Chicken Shreds',
-			species: { connect: { id: cat.id } },
-			unitType: 'Can',
-			unitsInStock: 72,
-			minFractionalAmount: 0.5
-		}
-	});
-
-	await prisma.food.create({
-		data: {
-			name: 'Friskies Prime Filets Turkey Dinner in Gravy',
-			species: { connect: { id: cat.id } },
-			unitType: 'Can',
-			unitsInStock: 28,
-			minFractionalAmount: 0.5
-		}
-	});
-
-	await prisma.food.create({
-		data: {
-			name: 'Crave High Protein Adult Salmon Dry Food',
-			species: { connect: { id: cat.id } },
-			unitType: 'Cup',
-			unitsInStock: 51.75,
-			minFractionalAmount: 0.25
-		}
-	});
+	/* TODO based on new schema
+	** 
+	**    
+	*/
 }
 
 main()
