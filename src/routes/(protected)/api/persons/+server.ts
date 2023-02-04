6067f1eb4121cf113daf891564a512a8df14dfca
Jon Croce <crocedev@gmail.com>
2023-02-03 23:27:25 -0500

    remove unnecessary API endpoints

--- src/routes/(protected)/api/persons/+server.ts
+++
@@ -1,9 +0,0 @@
-import { db } from '$lib/database';
-import { json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = async () => {
-	const persons = await db.person.findMany();
-
-	return json(persons);
-};
--- src/routes/(protected)/api/pets/+server.ts
+++
@@ -1,9 +0,0 @@
-import { db } from '$lib/database';
-import { json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = async () => {
-	const pets = await db.pet.findMany();
-
-	return json(pets);
-};
