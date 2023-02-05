c61c946a964f6c31c2f0b2e2dd7b8750b4662a80
Jon Croce <crocedev@gmail.com>
2023-02-05 00:14:39 -0500

    remove console.log statement

--- src/routes/(auth)/logout/+page.server.ts
+++ src/routes/(auth)/logout/+page.server.ts
@@ -3,7 +3,6 @@ import type { Actions } from './$types';
 
 export const actions: Actions = {
 	default({ cookies }) {
-		console.log(`in action to delete cookie on logout`);
 		cookies.delete('auth');
 		throw redirect(302, '/');
 	}
