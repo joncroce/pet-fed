cb9a102374447229b1c155584963b7b1b2a9c226
Jon Croce <crocedev@gmail.com>
2023-02-08 01:10:32 -0500

    refactor to use modal dialogs to display forms

--- src/routes/(protected)/dashboard/households/+page.svelte
+++ src/routes/(protected)/dashboard/households/+page.svelte
@@ -1,172 +1,187 @@
 <script lang="ts">
 	import '../dashboard.postcss';
+	import { writable, type Writable } from 'svelte/store';
 	import type { PageData } from './$types';
 	import Icon from '@iconify/svelte';
 	import CreateFoodForm from '$lib/components/CreateFoodForm.svelte';
+	import Modal, { bind } from 'svelte-simple-modal';
+	import type { Component } from 'svelte-simple-modal/types/Modal.svelte';
+	import EditHouseholdNameForm from '$lib/components/EditHouseholdNameForm.svelte';
+
+	const modal: Writable<Component | null> = writable(null);
+	const showCreateFoodModal = (householdId: string) =>
+		modal.set(
+			bind(CreateFoodForm, {
+				form: { household: householdId },
+				disableHouseholdSelect: true,
+				availableHouseholds: [],
+				successCallback: () => {
+					console.log('Successfully created food.');
+					modal.set(null);
+				},
+				cancel: () => modal.set(null),
+			}),
+		);
+
+	const showEditHouseholdNameModal = (householdId: string, householdName: string) => {
+		const household = { id: householdId, name: householdName };
+		return modal.set(
+			bind(EditHouseholdNameForm, {
+				form: { ...household },
+				household,
+				successCallback: () => {
+					console.log('Successfully updated household name.');
+					modal.set(null);
+				},
+				cancel: () => modal.set(null),
+			}),
+		);
+	};
 
 	export let data: PageData;
-
-	type Subgroup = 'pets' | 'persons' | 'foods';
-
-	$: formDisplayStatus = Object.fromEntries(
-		data.households.map((household) => [
-			household.id,
-			Object.fromEntries(['pets', 'persons', 'foods'].map((subgroup) => [subgroup, false])),
-		]),
-	);
-
-	function toggleForm(householdId: string, subgroupName: Subgroup) {
-		formDisplayStatus[householdId][subgroupName] = !formDisplayStatus[householdId][subgroupName];
-	}
 </script>
 
 <main>
-	<h2>Households</h2>
-	{#each data.households as household}
-		<article class="household">
-			<div class="household_header">
-				<h3 class="household_header_name">
-					{household.name}
-					{#if household.youAreManager}
-						<span class="household_header_name_role"
-							><Icon icon="material-symbols:star" />Manager<Icon
-								icon="material-symbols:star"
-							/></span
-						>
-					{/if}
-				</h3>
-				<a href="/dashboard/households/{household.id}/edit">Edit</a>
-			</div>
-			<section data-household-subgroup="pets">
-				<div class="household_subgroup_header">
-					<h4 class="household_subgroup_header_name">
-						Pets <span class="household_subgroup_header_count">({household.pets.length})</span>
-					</h4>
+	<Modal show={$modal}>
+		<h2>Households</h2>
+		{#each data.households as household}
+			<article class="household">
+				<div class="household_header">
+					<h3 class="household_header_name">
+						{household.name}
+						{#if household.youAreManager}
+							<span class="household_header_name_role"
+								><Icon icon="material-symbols:star" />Manager<Icon
+									icon="material-symbols:star"
+								/></span
+							>
+						{/if}
+					</h3>
 					<button
-						class={`button button-${formDisplayStatus[household.id]['pets'] ? 'close' : 'open'}`}
-						on:click={() => toggleForm(household.id, 'pets')}
-						type="button">{formDisplayStatus[household.id]['pets'] ? 'Cancel' : 'Add Pet'}</button
-					>
-				</div>
-				<table>
-					<thead>
-						<tr>
-							<th>Name</th>
-							<th>Is Present</th>
-						</tr>
-					</thead>
-					<tbody>
-						{#each household.pets as pet}
-							<tr>
-								<td><strong>{pet.name}</strong></td>
-								<td
-									>{#if pet.isPresent}<div class="icon-wrapper">
-											<Icon icon="material-symbols:check-box" />
-										</div>{/if}</td
-								>
-							</tr>
-						{/each}
-					</tbody>
-				</table>
-			</section>
-			<section data-household-subgroup="persons">
-				<div class="household_subgroup_header">
-					<h4 class="household_subgroup_header_name">
-						Persons <span class="household_subgroup_header_count">({household.persons.length})</span
-						>
-					</h4>
-					<button
-						class={`button button-${formDisplayStatus[household.id]['persons'] ? 'close' : 'open'}`}
-						on:click={() => toggleForm(household.id, 'persons')}
+						class="button with-icon"
 						type="button"
-						>{formDisplayStatus[household.id]['persons'] ? 'Cancel' : 'Add Person'}</button
+						on:click={() => showEditHouseholdNameModal(household.id, household.name)}
+						><Icon icon="material-symbols:edit" />Edit Name</button
 					>
 				</div>
-				<table>
-					<thead>
-						<tr>
-							<th>Name</th>
-							<th>Is Present</th>
-							<th>Is Manager</th>
-							<th>Invited By</th>
-							<th>Invite Status</th>
-						</tr>
-					</thead>
-					<tbody>
-						{#each household.persons as person}
-							<tr>
-								<td><strong>{person.displayName}</strong></td>
-								<td
-									>{#if person.isPresent}<div class="icon-wrapper">
-											<Icon icon="material-symbols:check-box" />
-										</div>{/if}</td
-								>
-								<td
-									>{#if person.isManager}<div class="icon-wrapper">
-											<Icon icon="material-symbols:check-box" />
-										</div>{/if}</td
-								>
-								<td>{person.invitedBy ?? '—'}</td>
-								<td
-									>{person.invitationAccepted === true
-										? 'Yes'
-										: person.invitationAccepted === false
-										? 'No'
-										: '—'}</td
-								>
-							</tr>
-						{/each}
-					</tbody>
-				</table>
-			</section>
-			<section data-household-subgroup="foods">
-				<div class="household_subgroup_header">
-					<h4 class="household_subgroup_header_name">
-						Foods <span class="household_subgroup_header_count">({household.foods.length})</span>
-					</h4>
-					<button
-						class={`button button-${formDisplayStatus[household.id]['foods'] ? 'close' : 'open'}`}
-						on:click={() => toggleForm(household.id, 'foods')}
-						type="button">{formDisplayStatus[household.id]['foods'] ? 'Cancel' : 'Add Food'}</button
-					>
-				</div>
-				{#if formDisplayStatus[household.id]['foods']}
-					<div class="household_subgroup_form-wrapper">
-						<CreateFoodForm
-							form={{ household: household.id }}
-							disableHouseholdSelect={true}
-							availableHouseholds={[]}
-							successCallback={() => {
-								toggleForm(household.id, 'foods');
-							}}
-							cancel={() => toggleForm(household.id, 'foods')}
-						/>
+				<section data-household-subgroup="pets">
+					<div class="household_subgroup_header">
+						<h4 class="household_subgroup_header_name">
+							Pets <span class="household_subgroup_header_count">({household.pets.length})</span>
+						</h4>
+						<div class="buttonset">
+							<button class="button" type="button" data-form-action="move">Bring Pet</button>
+							<button class="button" type="button" data-form-action="create">Create Pet</button>
+						</div>
 					</div>
-				{/if}
-				<table>
-					<thead>
-						<tr>
-							<th>Name</th>
-							<th>Qty in Stock</th>
-						</tr>
-					</thead>
-					<tbody>
-						{#each household.foods as food}
+					<table>
+						<thead>
 							<tr>
-								<td><strong>{food.name}</strong></td>
-								<td>{food.amountInStock} {food.unitName}</td>
-								<td
-									><a href={`/dashboard/foods/${food.id}`}
-										><Icon icon="material-symbols:edit-document" />Manage</a
-									></td
-								>
+								<th>Name</th>
+								<th>Is Present</th>
 							</tr>
-						{/each}
-					</tbody>
-				</table>
-			</section>
-		</article>
-	{/each}
+						</thead>
+						<tbody>
+							{#each household.pets as pet}
+								<tr>
+									<td><strong>{pet.name}</strong></td>
+									<td
+										>{#if pet.isPresent}<div class="icon-wrapper">
+												<Icon icon="material-symbols:check-box" />
+											</div>{/if}</td
+									>
+								</tr>
+							{/each}
+						</tbody>
+					</table>
+				</section>
+				<section data-household-subgroup="persons">
+					<div class="household_subgroup_header">
+						<h4 class="household_subgroup_header_name">
+							Persons <span class="household_subgroup_header_count"
+								>({household.persons.length})</span
+							>
+						</h4>
+						<button class="button" type="button" data-form-action="invite">Invite Person</button>
+					</div>
+					<table>
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>Is Present</th>
+								<th>Is Manager</th>
+								<th>Invited By</th>
+								<th>Invite Status</th>
+							</tr>
+						</thead>
+						<tbody>
+							{#each household.persons as person}
+								<tr>
+									<td><strong>{person.displayName}</strong></td>
+									<td>
+										{#if person.isPresent}
+											<div class="icon-wrapper">
+												<Icon icon="material-symbols:check-box" />
+											</div>
+										{/if}
+									</td>
+									<td>
+										{#if person.isManager}
+											<div class="icon-wrapper">
+												<Icon icon="material-symbols:check-box" />
+											</div>
+										{/if}
+									</td>
+									<td>{person.invitedBy ?? '—'}</td>
+									<td>
+										{person.invitationAccepted === true
+											? 'Yes'
+											: person.invitationAccepted === false
+											? 'No'
+											: '—'}
+									</td>
+								</tr>
+							{/each}
+						</tbody>
+					</table>
+				</section>
+				<section data-household-subgroup="foods">
+					<div class="household_subgroup_header">
+						<h4 class="household_subgroup_header_name">
+							Foods <span class="household_subgroup_header_count">({household.foods.length})</span>
+						</h4>
+						<button
+							class="button"
+							type="button"
+							data-form-action="create"
+							on:click={() => showCreateFoodModal(household.id)}>Create Food</button
+						>
+					</div>
+					<table>
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>Qty in Stock</th>
+							</tr>
+						</thead>
+						<tbody>
+							{#each household.foods as food}
+								<tr>
+									<td><strong>{food.name}</strong></td>
+									<td>{food.amountInStock} {food.unitName}</td>
+									<td
+										><button class="button with-icon" type="button" data-form-action="edit"
+											><Icon icon="material-symbols:edit-document" />Edit</button
+										></td
+									>
+								</tr>
+							{/each}
+						</tbody>
+					</table>
+				</section>
+			</article>
+		{/each}
+	</Modal>
 </main>
 
 <style lang="postcss">
@@ -206,23 +221,30 @@
 
 				.button {
 					font-size: 1.2rem;
-
-					&-close {
-						background-color: var(--color-red);
-					}
-
-					&-open {
-						background-color: var(--color-green);
-					}
 				}
-			}
-
-			&_form-wrapper {
 			}
 		}
 
 		section {
 			margin-block: 2rem;
 		}
+	}
+
+	td:has(button) {
+		button {
+			font-size: 0.9em;
+		}
+	}
+
+	.buttonset {
+		display: flex;
+		gap: 1em;
+	}
+
+	button.with-icon {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 0.25em;
 	}
 </style>
